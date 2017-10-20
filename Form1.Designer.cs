e7e0411c14e243c8c26dc1c235187a6de30848e6
Nikolay91m <32764978+Nikolay91m@users.noreply.github.com>
2017-10-20 12:53:25 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ExcelToOracle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fillDBbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openXLSXbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSourceTextBox = new System.Windows.Forms.TextBox();
+            this.userIDTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chosenFileTextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fillDBbutton
+            // 
+            this.fillDBbutton.Enabled = false;
+            this.fillDBbutton.Location = new System.Drawing.Point(15, 322);
+            this.fillDBbutton.Name = "fillDBbutton";
+            this.fillDBbutton.Size = new System.Drawing.Size(343, 23);
+            this.fillDBbutton.TabIndex = 0;
+            this.fillDBbutton.Text = "Создать и заполнить БД";
+            this.fillDBbutton.UseVisualStyleBackColor = true;
+            this.fillDBbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Data Source:";
+            // 
+            // openXLSXbutton
+            // 
+            this.openXLSXbutton.Location = new System.Drawing.Point(283, 8);
+            this.openXLSXbutton.Name = "openXLSXbutton";
+            this.openXLSXbutton.Size = new System.Drawing.Size(75, 23);
+            this.openXLSXbutton.TabIndex = 3;
+            this.openXLSXbutton.Text = "Выбрать";
+            this.openXLSXbutton.UseVisualStyleBackColor = true;
+            this.openXLSXbutton.Click += new System.EventHandler(this.openXLSXbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Выберите xlsx файл:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(346, 176);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // dataSourceTextBox
+            // 
+            this.dataSourceTextBox.Location = new System.Drawing.Point(85, 244);
+            this.dataSourceTextBox.Name = "dataSourceTextBox";
+            this.dataSourceTextBox.Size = new System.Drawing.Size(270, 20);
+            this.dataSourceTextBox.TabIndex = 6;
+            this.dataSourceTextBox.Text = "XE";
+            // 
+            // userIDTextBox
+            // 
+            this.userIDTextBox.Location = new System.Drawing.Point(85, 270);
+            this.userIDTextBox.Name = "userIDTextBox";
+            this.userIDTextBox.Size = new System.Drawing.Size(270, 20);
+            this.userIDTextBox.TabIndex = 8;
+            this.userIDTextBox.Text = "system";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "User ID:";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(85, 296);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(270, 20);
+            this.passwordTextBox.TabIndex = 10;
+            this.passwordTextBox.Text = "test";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Password:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 228);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Настройки подключения Oracle:";
+            // 
+            // chosenFileTextBox
+            // 
+            this.chosenFileTextBox.Enabled = false;
+            this.chosenFileTextBox.Location = new System.Drawing.Point(127, 9);
+            this.chosenFileTextBox.Name = "chosenFileTextBox";
+            this.chosenFileTextBox.Size = new System.Drawing.Size(150, 20);
+            this.chosenFileTextBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 355);
+            this.Controls.Add(this.chosenFileTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.userIDTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataSourceTextBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openXLSXbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fillDBbutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fillDBbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openXLSXbutton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox dataSourceTextBox;
+        private System.Windows.Forms.TextBox userIDTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox chosenFileTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+using OfficeOpenXml;
+using System.IO;
+using System.Text.RegularExpressions; 
+
+namespace ExcelToOracle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string oradb = String.Format("Data Source={0};User Id={1};Password={2};",
+                    dataSourceTextBox.Text, userIDTextBox.Text, passwordTextBox.Text);
+                using (var con = new OracleConnection(oradb))
+                {
+                    con.Open();
+                    OracleCommand cmd = new OracleCommand();
+                    cmd.Connection = con;
+                    StringBuilder sbCreate = new StringBuilder();
+                    StringBuilder sbInsert = new StringBuilder();
+                    sbCreate.Append(@"CREATE TABLE excel ( id NUMBER PRIMARY KEY");
+                    sbInsert.Append(@"INSERT INTO excel VALUES (:id");
+                    for (int rows = 0; rows < dataGridView1.Rows.Count; rows++)
+                    {
+                        string titleRow = dataGridView1.Rows[rows].Cells[1].Value.ToString();
+                        string typeRow = dataGridView1.Rows[rows].Cells[2].Value.ToString(); 
+                        sbCreate.Append(", ");
+                        sbCreate.Append(titleRow);
+                        sbCreate.Append(" ");
+                        sbCreate.Append(typeRow);
+                        sbInsert.Append(", :");
+                        sbInsert.Append(titleRow);
+                    }
+                    sbCreate.Append(")");
+                    sbInsert.Append(")");
+                    cmd.CommandText = sbCreate.ToString();
+                    cmd.CommandType = CommandType.Text;
+                    cmd.ExecuteNonQuery();
+
+                    using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(chosenFileTextBox.Text)))
+                    {
+                        var myWorksheet = xlPackage.Workbook.Worksheets.First();
+                        var totalRows = myWorksheet.Dimension.End.Row;
+                        var totalColumns = myWorksheet.Dimension.End.Column;
+
+                        var titles = myWorksheet.Cells[1, 1, 1, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+
+                        for (int rowNum = 2; rowNum <= totalRows; rowNum++)
+                        {
+                            var cells = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+
+                            var sql = sbInsert.ToString();
+                            using (var cmdInsert = new OracleCommand(sql, con))
+                            {
+                                List<OracleParameter> parameters = new List<OracleParameter>();
+                                parameters.Add(new OracleParameter("id", rowNum-1));
+                                for (int i = 0; i < titles.Count(); i++)
+                                {
+                                    OracleParameter op = new OracleParameter();
+                                    op.ParameterName = titles.ElementAt(i);
+                                    string typeRow = dataGridView1.Rows[i].Cells[2].Value.ToString();
+                                    if (typeRow == "NUMBER") //Сделать больше типов, сделать покрасивее, разобраться с типами Oracle
+                                    {
+                                        op.Value = Convert.ToInt32(cells.ElementAt(i));
+                                        op.DbType = DbType.Int32;
+                                    }
+                                    else if (typeRow == "VARCHAR(255)")
+                                    {
+                                        op.Value = cells.ElementAt(i);
+                                        op.DbType = DbType.String;
+                                    }
+                                    parameters.Add(op);
+                                }
+                                cmdInsert.Parameters.AddRange(parameters.ToArray());
+                                cmdInsert.ExecuteNonQuery();
+                            }
+
+                        }
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Ошибка: " + exp.Message);
+            }
+
+        }
+
+        private void openXLSXbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "xlsx files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(openFileDialog.FileName)))
+                    {
+                        var myWorksheet = xlPackage.Workbook.Worksheets.First();
+                        var totalColumns = myWorksheet.Dimension.End.Column;
+                        var titles = myWorksheet.Cells[1, 1, 1, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                        var values = myWorksheet.Cells[2, 1, 2, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+
+                        var dt = new DataTable();
+                        dt.Columns.Add(new DataColumn("Выбрать", typeof(bool)));
+                        dt.Columns.Add(new DataColumn("Название столбца в Excel", typeof(string)));
+                        dt.Columns.Add(new DataColumn("Тип столбца в ORACLE", typeof(string)));
+                        for (int i = 0; i < titles.Count(); i++ )
+                        {
+                            DataRow datarow = dt.NewRow();
+                            datarow["Выбрать"] = true;
+                            datarow["Название столбца в Excel"] = titles.ElementAt(i);
+                            if (Regex.IsMatch(values.ElementAt(i), @"^\d+$"))
+                                datarow["Тип столбца в ORACLE"] = "NUMBER";
+                            else
+                                datarow["Тип столбца в ORACLE"] = "VARCHAR(255)";
+                            dt.Rows.Add(datarow);
+                        }
+                        dataGridView1.DataSource = dt;
+                        fillDBbutton.Enabled = true;
+                        chosenFileTextBox.Text = openFileDialog.FileName;
+                    }
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Ошибка: " + exp.Message);
+                }
+            }
+        }
+    }
+}
